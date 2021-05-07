982f5b94c57e06bf41a1013f04c1856cb597883b
JMalone <jlmalone9291@gmail.com>
2021-05-06 21:09:19 -0400

    Game Loop

--- app/src/main/java/com/example/a2dbattlegame/MainActivity.java
+++ app/src/main/java/com/example/a2dbattlegame/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.a2dbattlegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //Set window to fullscreen (Hide status bar)
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        //Set content view to game, so that objects in the game class can be rendered
+        setContentView(new Game(this));
     }
 }
