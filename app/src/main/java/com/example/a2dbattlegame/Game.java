92086614e0f61755d5946a204b2ea7d290b81fd0
JMalone <jlmalone9291@gmail.com>
2021-05-06 22:44:16 -0400

    added all files that include new player class and touch events

--- app/src/main/java/com/example/a2dbattlegame/Game.java
+++ app/src/main/java/com/example/a2dbattlegame/Game.java
@@ -3,6 +3,7 @@ package com.example.a2dbattlegame;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -16,18 +17,42 @@ import androidx.core.content.ContextCompat;
 * */
 
 public class Game extends SurfaceView implements SurfaceHolder.Callback {
+    private final Player player;
     private GameLoop gameLoop;
-    private Context context;
+    //private Context context;
 
     public Game(Context context) {
         super(context);
-
+        //getContext();
         SurfaceHolder surfaceHolder = getHolder();
         surfaceHolder.addCallback(this);
 
-        this. context = context;
+        //this. context = context;
         gameLoop = new GameLoop(this, surfaceHolder);
+
+        //Initialize Player
+        player = new Player(getContext(), 2*500, 500, 30);
+
         setFocusable(true);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        //Handle different types of touch events actions
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                player.setPostion((double) event.getX(), (double) event.getY());
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                player.setPostion((double) event.getX(), (double) event.getY());
+                return true;
+
+        }
+
+
+        return super.onTouchEvent(event);
     }
 
     @Override
@@ -51,12 +76,13 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
         drawUPS(canvas);
         drawFPS(canvas);
 
+        player.draw(canvas);
     }
 
     public void drawUPS(Canvas canvas){
         String averageUPS = Double.toString(gameLoop.getAverageUPS());
         Paint paint = new Paint();
-        int color = ContextCompat.getColor(context, R.color.magenta);
+        int color = ContextCompat.getColor(getContext(), R.color.magenta);
         paint.setColor(color);
         paint.setTextSize(50);
         canvas.drawText("UPS: " + averageUPS, 100, 100, paint);
@@ -64,7 +90,7 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
     public void drawFPS(Canvas canvas){
         String averageFPS = Double.toString(gameLoop.getAverageFPS());
         Paint paint = new Paint();
-        int color = ContextCompat.getColor(context, R.color.magenta);
+        int color = ContextCompat.getColor(getContext(), R.color.magenta);
         paint.setColor(color);
         paint.setTextSize(50);
         canvas.drawText("FPS: " + averageFPS, 100, 200, paint);
@@ -72,5 +98,6 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
 
     public void update() {
         // Update game state
+        player.update();
     }
 }
--- app/src/main/java/com/example/a2dbattlegame/Player.java
+++ app/src/main/java/com/example/a2dbattlegame/Player.java
@@ -1,4 +1,37 @@
 package com.example.a2dbattlegame;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import androidx.core.content.ContextCompat;
+
 public class Player {
+    private double positionx;
+    private double positiony;
+    private double radius;
+    private Paint paint;
+
+    public Player(Context context, double positionx, double positiony, double radius){
+        this.positionx = positionx;
+        this.positiony = positiony;
+        this.radius = radius;
+
+        paint = new Paint();
+        int color = ContextCompat.getColor(context, R.color.player);
+        paint.setColor(color);
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawCircle((float) positionx, (float) positiony, (float) radius, paint);
+        
+    }
+
+    public void update() {
+    }
+
+    public void setPostion(double positionx, double positiony) {
+        this.positiony = positiony;
+        this.positionx = positionx;
+    }
 }
